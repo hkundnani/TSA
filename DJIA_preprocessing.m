0c285ce31fe0000795a23987cb1d3741bc6a2cdd
jarvisaltrin <jarvisaltrin@gmail.com>
2017-10-18 22:51:24 -0400

    DJIA Preprocessing Completed
    Adjusted Close Value for AAPL generated. Saved in AAPL file with missing
    dates.
    Preprocessed File Format:
    Col 1 - 1 if date present, 0 for newly added date
    Col 2 - Adj Close Value
    Col 3 - Dates

--- DJIA_preprocessing.m
+++ DJIA_preprocessing.m
@@ -10,3 +10,34 @@ Adj_close = table2array(T(:,6));
 Volume = table2array(T(:,7));
 length = numel(Open(:,1));
 
+%preproccessing Date
+D=datevec(Date);
+D(:,2)=D(:,5);
+D(:,5)=zeros;
+Date=datetime(D);
+startdate = Date(1,1);
+enddate = Date(length,1);
+filledDate = startdate:enddate;
+filledDate = filledDate'; %transpose - convert row to column matrix
+
+count = 1;
+for i=1:numel(filledDate(:,1))
+    if (filledDate(i,1) == Date(count,1))
+        visited(i,1) = 1;
+        visited(i,2) = Adj_close(count,1);
+		count = count + 1;
+	else
+		visited(i,2) = -1;
+        Adj1 = visited(i-1,2);
+		Adj2 = Adj_close(count,1);
+		avg = (Adj1 + Adj2)/2;
+		visited(i,2) = avg;
+    end
+end
+
+filledDate.Format = 'yyyy-MM-dd';
+newdata = [num2cell(visited) cellstr(filledDate)];
+dlmcell('AAPL',newdata,'\t');
+
+%T1 = readtable('AAPL');
+
