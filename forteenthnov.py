adfc15453948048f615ed42dd8f3d6c54450ebe4
ankitajaiwar <32300972+ankitajaiwar@users.noreply.github.com>
2017-11-30 14:34:22 -0500

    updated

--- forteenthnov.py
+++ forteenthnov.py
@@ -86,16 +86,39 @@ result['Label'].fillna(0.0, inplace = True)
 result.dropna(axis=0, inplace = True)
 
 
-train,test = train_test_split(result,test_size=0.2,random_state=10)# about random_state
+c =[]
 
 
-compound_list = train['Compound'].tolist()
-label_list = train['Compound'].tolist()
+complist = result['Compound'].tolist()
+for i in range(len(complist)):
+    if i<3:
+        c.append(0.0)
+    if i >3:
+        val= (complist[i-1]+complist[i-2]+complist[i-3])/3
+        c.append(val)
+        
+date_list = result['Date'].tolist()
+adj_list = result['Adjvalue'].tolist()
+label_list = result['Label'].tolist()
+final_list = zip(date_list, adj_list, label_list, complist, c)
+label = ['Date', 'Adjvalue', 'Label', 'Compound', 'Prev3comp']
+final_dframe = pd.DataFrame.from_records(final_list, columns = label)
 
-fit = classifier.fit(train['Compound'].reshape(len(train['Compound']),1), train['Label'])
+train, test = train_test_split(final_dframe, test_size=0.3, random_state=10)
+
+
+
+        
+
+        
+        
+
+
+fit = classifier.fit(train['Prev3comp'].reshape(len(train['Prev3comp']),1), train['Label'])
 y_pred = classifier.predict(test['Label'].reshape(len(test['Label']),1))
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(classifier.score(test['Compound'].reshape(len(test['Compound']),1),test['Label'].reshape(len(test['Label']),1) )))
-
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(classifier.score(test['Prev3comp'].reshape(len(test['Prev3comp']),1),test['Label'].reshape(len(test['Label']),1) )))
+accuracy = accuracy_score(y_pred,test['Label'].reshape(len(test['Label'])))
+print(accuracy)
 
 
 
